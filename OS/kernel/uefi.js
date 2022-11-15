ef6c03a4ceb9dacb0a710fc3a7e26bc75d9a8ca0
SithLor <84148006+SithLor@users.noreply.github.com>
2022-11-15 22:07:28 +0000

    zam

--- OS/kernel/uefi.js
+++ OS/kernel/uefi.js
@@ -1,6 +1,6 @@
-import { KernelCpuInfo } from "./DrvFunction/bios/Cpu/getCpuInfo.js";
-import { KernelCpuEndian } from "./DrvFunction/bios/Cpu/getCpuendian.js.js";
-import { KernelCpuArch } from "./DrvFunction/bios/Cpu/getCpuArch.js.js";
-import { KernelRamAmount } from "./DrvFunction/bios/memory/GetAllram.js";
-import { KernelRamFreeMemory } from "./DrvFunction/bios/memory/getfreeMemory.js.js";
-import { KernelGetBiosEnvioment } from "./DrvFunction/bios/Cpu/getBiosEnvioment.js";
+import { KernelCpuInfo } from "./drv/function/bios/Cpu/getCpuInfo.js.js";
+import { KernelCpuEndian } from "./drv/function/bios/Cpu/getCpuendian.js.js.js";
+import { KernelCpuArch } from "./drv/function/bios/Cpu/getCpuArch.js.js.js";
+import { KernelRamAmount } from "./drv/function/bios/memory/GetAllram.js";
+import { KernelRamFreeMemory } from "./drv/function/bios/memory/getfreeMemory.js.js.js";
+import { KernelGetBiosEnvioment } from "./drv/function/bios/Cpu/getBiosEnvioment.js.js";
