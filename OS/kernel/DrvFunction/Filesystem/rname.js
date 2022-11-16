a957ce94f76008daec215b0ca2ebfdf7caf843b9
SithLor <84148006+SithLor@users.noreply.github.com>
2022-11-16 15:37:24 +0000

    yes

--- OS/kernel/DrvFunction/Filesystem/rname.js
+++
@@ -1,6 +0,0 @@
-import { FileSystem } from "../../../imports/FileSystem";
-export function KernelTFSRenameFile(Target, Name) {
-  FileSystem.rename(Target, Name, function (err) {
-    if (err) throw err;
-  });
-}
--- OS/kernel/DrvFunction/Filesystem/writefile.js
+++
@@ -1,7 +0,0 @@
-
-import { FileSystem } from "../../../imports/FileSystem";
-export function KernelTFSWriteFile(FILENAME, FILEEXTENION, DATA) {
-  FileSystem.writeFile(`${FILENAME}${FILEEXTENION}`, DATA, function (err) {
-    if (err) throw err;
-  });
-}
